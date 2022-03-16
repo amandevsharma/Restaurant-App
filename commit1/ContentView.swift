ebf2ba9d8b423cf2cd62264591f2d6e5a5fd4013
Aman Sharma <98018329+amandevsharma@users.noreply.github.com>
2022-03-16 15:47:40 -0400

    📖 read me file

---
+++ commit1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  commit1
+//
+//  Created by Aman Sharma on 3/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, universe!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

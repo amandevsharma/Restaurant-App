00cd3d42eaf0e33ad711807a34eb9c36a91528e8
Aman Sharma <98018329+amandevsharma@users.noreply.github.com>
2022-03-16 16:11:23 -0400

    added tab view

---
+++ commit1/TabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  commit1
+//
+//  Created by Aman Sharma on 3/16/22.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text("Hello, www!")
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
